fbd466eba9f380bf71193690cc2a297b39f5f81e
ndunnett <nathandunnett@me.com>
2024-02-22 04:43:13 +0000

    implement exception handling

--- app/blocks/__init__.py
+++ app/blocks/__init__.py
@@ -1,5 +1,9 @@
-from quart import Blueprint, render_template, request, current_app, make_response
+from quart import Blueprint, render_template, Request, request, current_app
+from werkzeug.exceptions import NotFound
+from functools import cache
 from app.blocks.graphing import create_dot
+from app.models import *
+from app.utilities import serve_plain_text
 
 
 bp = Blueprint("blocks", __name__, template_folder="templates", static_folder="static", url_prefix="/blocks")
@@ -7,26 +11,37 @@ bp = Blueprint("blocks", __name__, template_folder="templates", static_folder="s
 navbar = {}
 
 
+@cache
+def get_obj(compound: str, block: str) -> Block:
+    """Retrieve block object from application state and handle 'not found' error"""
+    data: Data = current_app.data
+    if (obj := data.get_block(compound, block)):
+        return obj
+    else:
+        if compound in data.compounds:
+            raise NotFound(f"Block \"{block}\" not found in compound \"{compound}\".")
+        else:
+            raise NotFound(f"Compound \"{compound}\" not found.")
+
+
+def get_depth(request: Request) -> int:
+    """Retrieve and validate depth parameter from request args"""
+    return max(0, min(5, request.args.get("depth", default=1, type=int)))
+
+
 @bp.route("/<compound>/<block>/detail")
 async def view_detail(compound: str, block: str):
     """View block detail"""
-    obj = current_app.data.get_block(compound, block)
-    return await render_template("view_detail.html.j2", compound=obj.compound, block=obj.name, obj=obj)
+    return await render_template("view_detail.html.j2", obj=get_obj(compound, block))
 
 
 @bp.route("/<compound>/<block>/diagram")
 async def view_diagram(compound: str, block: str):
     """View diagram using d3-graphviz to render .svg in WebAssembly"""
-    obj = current_app.data.get_block(compound, block)
-    depth = max(0, min(5, request.args.get("depth", default=1, type=int)))
-    return await render_template("view_diagram.html.j2", obj=obj, depth=depth)
+    return await render_template("view_diagram.html.j2", obj=get_obj(compound, block), depth=get_depth(request))
 
 
 @bp.route("/<compound>/<block>/dot")
 async def dot(compound: str, block: str):
     """Construct diagram in DOT format and serve as plain text"""
-    obj = current_app.data.get_block(compound, block)
-    depth = max(0, min(5, request.args.get("depth", default=1, type=int)))
-    response = await make_response(await create_dot(obj, depth), 200)
-    response.mimetype = "text/plain"
-    return response
+    return await serve_plain_text(await create_dot(get_obj(compound, block), get_depth(request)))
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,7 +1,9 @@
 from quart import Blueprint, render_template
+from werkzeug.exceptions import HTTPException, InternalServerError
 
 
-bp = Blueprint("main", __name__, template_folder="templates", static_folder="static", url_prefix="/", static_url_path="/main/static")
+bp = Blueprint("main", __name__, template_folder="templates", static_folder="static",
+               url_prefix="/", static_url_path="/main/static")
 
 navbar = {
     "main.home": "Home",
@@ -12,3 +14,12 @@ navbar = {
 async def home():
     """Home page"""
     return await render_template("home.html.j2", page_name="main.home")
+
+
+@bp.app_errorhandler(Exception)
+async def exception_handler(e):
+    """Generic app wide exception handler"""
+    if isinstance(e, HTTPException):
+        return await render_template("error.html.j2", error=e), e.code
+    else:
+        return await render_template("error.html.j2", error=InternalServerError), 500
