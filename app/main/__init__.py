0a834523d7bd839ea48b7f75df6c463c524505e6
ndunnett <nathandunnett@me.com>
2024-02-22 22:57:12 +0000

    improve exception handling

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,6 @@
-from quart import Blueprint, render_template
+from quart import Blueprint, render_template, current_app
 from werkzeug.exceptions import HTTPException, InternalServerError
+import traceback
 
 
 bp = Blueprint("main", __name__, template_folder="templates", static_folder="static",
@@ -16,10 +17,27 @@ async def home():
     return await render_template("home.html.j2", page_name="main.home")
 
 
+def log_exception(e: Exception) -> None:
+    """Output formatted stack trace to app logger as an error"""
+    tb = traceback.format_exception(e.__class__, e, e.__traceback__)
+    formatted = "\n".join(map(lambda s: " " * 2 + s, "".join(tb).splitlines()))
+    current_app.logger.error("\n" + formatted)
+
+
+async def render_exception(e: Exception):
+    """Render template to display exception page"""
+    return await render_template("error.html.j2", error=e), e.code
+
+
+@bp.app_errorhandler(HTTPException)
+async def http_exception_handler(e):
+    """HTTP exception handler"""
+    log_exception(e)
+    return await render_exception(e)
+
+
 @bp.app_errorhandler(Exception)
-async def exception_handler(e):
-    """Generic app wide exception handler"""
-    if isinstance(e, HTTPException):
-        return await render_template("error.html.j2", error=e), e.code
-    else:
-        return await render_template("error.html.j2", error=InternalServerError), 500
+async def generic_exception_handler(e):
+    """Generic server error exception handler"""
+    log_exception(e)
+    return await render_exception(InternalServerError(original_exception=e))
