a7f65eb1dfc2fd90fa41eb8d405711a08740222c
ndunnett <nathandunnett@me.com>
2024-10-01 23:14:48 +0000

    add docstrings

--- src/pyfastmurmur3/fastmurmur3.pyi
+++ src/pyfastmurmur3/fastmurmur3.pyi
@@ -1,1 +1,11 @@
-def hash(text: str) -> int: ...
+"""
+This module is a thin wrapper around the Rust crate [fastmurmur3](https://crates.io/crates/fastmurmur3) which is an implementation of the [MurmurHash3 algorithm](https://en.wikipedia.org/wiki/MurmurHash).
+"""
+
+def hash(text: str) -> int:
+    """Fast, non-cryptographic hash function.
+
+    .. code-block:: python
+        foo = fastmurmur3.hash("bar")
+    """
+    ...
